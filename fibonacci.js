a86401bd3bd055f294d0f85ea4be924a065ed77e
isaacBejarano <isaacbejaranopro@gmail.com>
2020-06-01 03:08:48 +0200

    script

---
+++ fibonacci.js
@@ -0,0 +1,14 @@
+// lib
+function fibonacci(value) {
+	let sequence = [0, 1]; // value > 2
+
+	if (value >= 0 && value < 2) {
+		sequence = [0];
+	} else {
+		// value > 2
+		for (let i = 2; i < value; i++) {
+			sequence[i] = sequence[i - 1] + sequence[i - 2]; // push sum
+		}
+	}
+	return sequence;
+}
